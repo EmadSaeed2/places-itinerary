fd4d1980acdfb89893b6c27f3c4cf96ba7b7c8e1
Emad Saeed <eaf.saeed@gmail.com>
2023-01-06 14:55:46 +0000

    Include Places API - JS

--- assets/js/autocomplete.js
+++ assets/js/autocomplete.js
@@ -1,6 +1,7 @@
 var city;
 var photosArr;
 var autocomplete;
+
 function initAutocomplete() {
     autocomplete = new google.maps.places.Autocomplete(
         document.querySelector('.autocomplete'), {
