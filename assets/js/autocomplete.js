0b2f4b0b655fad2697188aa70dab585fa4030cdd
Emad Saeed <eaf.saeed@gmail.com>
2023-01-06 00:13:00 +0000

    onPlaceChanged

--- assets/js/autocomplete.js
+++ assets/js/autocomplete.js
@@ -9,4 +9,20 @@ function initAutocomplete() {
     autocomplete.addListener('place_changed', onPlaceChanged);
 }
 
+function onPlaceChanged() {
+    var placeData = autocomplete.getPlace();
 
+    if (!placeData.geometry) { //validate the user input
+        document.querySelector('.autocomplete').placeholder = 'Please enter and select a city';
+        console.log('invalid input')
+    } else {
+        // console.log(placeData);
+        city = document.querySelector('.autocomplete').value;
+        console.log(city);
+
+        photosArr = placeData.photos;
+        for (var photo of photosArr) {
+            console.log(photo.getUrl());
+        }
+    }
+}
